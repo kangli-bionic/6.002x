f8d5cd55f7fabdd2c46da91936ba68525bae7b82
Peter Tia <aznboypmt@yahoo.com>
2015-08-15 22:54:44 -0400

    completed Robot class
    completed Robot class

--- week2/pset2.py
+++ week2/pset2.py
@@ -87,3 +87,75 @@ class RectangularRoom(object):
             return True
         else:
             return False
+
+class Robot(object):
+    """
+    Represents a robot cleaning a particular room.
+
+    At all times the robot has a particular position and direction in the room.
+    The robot also has a fixed speed.
+
+    Subclasses of Robot should provide movement strategies by implementing
+    updatePositionAndClean(), which simulates a single time-step.
+    """
+    def __init__(self, room, speed):
+        """
+        Initializes a Robot with the given speed in the specified room. The
+        robot initially has a random direction and a random position in the
+        room. The robot cleans the tile it is on.
+
+        room:  a RectangularRoom object.
+        speed: a float (speed > 0)
+        """
+        self.room = room ## do we declare room is an object of RectangularRoom?
+        ## we are told room is a RectangularRoom object
+        ## i guess it should be created somewhere else where room = RectangularRoom()
+        self.speed = speed
+        self.direction = random.randint(0,359)
+        self.position = room.getRandomPosition()  #performs getRandomPosition
+        # how do we know size of room?
+        # init means the size of the room must be given when the object is created?
+        
+
+    def getRobotPosition(self):
+        """
+        Return the position of the robot.
+
+        returns: a Position object giving the robot's position.
+        """
+        return  self.position
+    
+    def getRobotDirection(self):
+        """
+        Return the direction of the robot.
+
+        returns: an integer d giving the direction of the robot as an angle in
+        degrees, 0 <= d < 360.
+        """
+        return self.direction
+
+    def setRobotPosition(self, position):
+        """
+        Set the position of the robot to POSITION.
+
+        position: a Position object.
+        """
+        self.position = position
+
+    def setRobotDirection(self, direction):
+        """
+        Set the direction of the robot to DIRECTION.
+
+        direction: integer representing an angle in degrees
+        """
+        self.direction = direction
+
+    def updatePositionAndClean(self):
+        """
+        Simulate the raise passage of a single time-step.
+
+        Move the robot to a new position and mark the tile it is on as having
+        been cleaned.
+        """
+        raise NotImplementedError # don't change this!
+
